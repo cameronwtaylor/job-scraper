749f85d6f4df92bf8845e38acdac98848e7a9a96
Cameron Taylor <cameronwaynetaylor@gmail.com>
2022-03-26 17:11:35 -0400

    Add job json op

--- gitlab-scraper.py
+++ gitlab-scraper.py
@@ -58,6 +58,25 @@ def build_department_json(department_level_one_list, department_level_two_list, 
     with open('gitlab_departments.json', 'w') as fp:
         json.dump(department_list, fp, sort_keys=True, indent=4)
 
+@op
+def build_job_json(soup):
+    job_list = []
+    jobs = soup.find_all('div', {'class':'opening'})
+    for job in jobs:
+        job_title_value = job.find('a').text
+        job_href_value = job.find('a').get('href')
+        job_department_id_value = job.get('department_id')
+        job_office_id_value = job.get('office_id')
+        job_location_value = job.find('span', {'class':'location'}).text
+        job_dict = {'job_title':job_title_value,
+                    'job_href':job_href_value,
+                    'job_department_id':job_department_id_value,
+                    'job_office_id':job_office_id_value,
+                    'job_location':job_location_value}
+        job_list.append(job_dict)
+    with open('gitlab_jobs.json', 'w') as fp:
+        json.dump(job_list, fp, sort_keys=True, indent=4)
+
 @job(
     resource_defs={'io_manager': mem_io_manager},
     executor_def=in_process_executor
@@ -74,21 +93,4 @@ def gitlab_jobs_extract():
     department_level_two_list = create_list_of_departments(soup, tag_h4, tag_h3, department_level_two)
     department_level_three_list = create_list_of_departments(soup, tag_h5, tag_h4, department_level_three) 
     build_department_json(department_level_one_list, department_level_two_list, department_level_three_list)
-
-""" job_list = []
-jobs = soup.find_all('div', {'class':'opening'})
-for job in jobs:
-    job_title_value = job.find('a').text
-    job_href_value = job.find('a').get('href')
-    job_department_id_value = job.get('department_id')
-    job_office_id_value = job.get('office_id')
-    job_location_value = job.find('span', {'class':'location'}).text
-    job_dict = {'job_title':job_title_value,
-                'job_href':job_href_value,
-                'job_department_id':job_department_id_value,
-                'job_office_id':job_office_id_value,
-                'job_location':job_location_value}
-    job_list.append(job_dict)
-
-with open('gitlab_jobs.json', 'w') as fp:
-   json.dump(job_list, fp, sort_keys=True, indent=4) """
+    build_job_json(soup)
