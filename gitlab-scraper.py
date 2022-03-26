e69a7cb821e6723581b1eec3c33bb41758580572
Cameron Taylor <cameronwaynetaylor@gmail.com>
2022-03-26 17:16:32 -0400

    Refactor ops and job

--- gitlab-scraper.py
+++ gitlab-scraper.py
@@ -10,31 +10,6 @@ def retrieve_job_board_soup():
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
-@op
-def set_tag_h3():
-    return 'h3'
-
-@op
-def set_tag_h4():
-    return 'h4'
-
-@op
-def set_tag_h5():
-    return 'h5'
-
-@op
-def set_department_level_one():
-    return 'one'
-
-@op
-def set_department_level_two():
-    return 'two'
-
-@op
-def set_department_level_three():
-    return 'three'
-
-@op
 def create_list_of_departments(soup, tag, previous_tag, department_level):
     department_list = []
     departments = soup.find_all(tag)
@@ -53,7 +28,10 @@ def create_list_of_departments(soup, tag, previous_tag, department_level):
     return list(filter(lambda i: i['department_id'] != 'filter-count', department_list)) 
 
 @op
-def build_department_json(department_level_one_list, department_level_two_list, department_level_three_list):
+def build_department_json(soup):
+    department_level_one_list = create_list_of_departments(soup, 'h3', 'h3', 'one')
+    department_level_two_list = create_list_of_departments(soup, 'h4', 'h3', 'two')
+    department_level_three_list = create_list_of_departments(soup, 'h5', 'h4', 'three')
     department_list = department_level_one_list + department_level_two_list + department_level_three_list
     with open('gitlab_departments.json', 'w') as fp:
         json.dump(department_list, fp, sort_keys=True, indent=4)
@@ -83,14 +61,5 @@ def build_job_json(soup):
 )
 def gitlab_jobs_extract():
     soup = retrieve_job_board_soup()
-    tag_h3 = set_tag_h3()
-    tag_h4 = set_tag_h4()
-    tag_h5 = set_tag_h5()
-    department_level_one = set_department_level_one()
-    department_level_two = set_department_level_two()
-    department_level_three = set_department_level_three()
-    department_level_one_list = create_list_of_departments(soup, tag_h3, tag_h3, department_level_one)
-    department_level_two_list = create_list_of_departments(soup, tag_h4, tag_h3, department_level_two)
-    department_level_three_list = create_list_of_departments(soup, tag_h5, tag_h4, department_level_three) 
-    build_department_json(department_level_one_list, department_level_two_list, department_level_three_list)
+    build_department_json(soup)
     build_job_json(soup)
